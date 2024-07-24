83b0845474e767b7376b82f57c56d7950fe92830
Nam PT <namfet@gmail.com>
2024-07-24 22:23:39 +0700

    make main.py
    tạo hàm add

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+
+def add(a, b):
+    return a + b
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    a, b = sys.argv[1], sys.argv[2] # python execute param
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
