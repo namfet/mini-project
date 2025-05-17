14b2e891c75d997b71093c349d0db744f0e5ac1e
Nam PT <namfet@gmail.com>
2025-05-17 21:52:00 +0700

    make main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import sys
+
+def add(a, b):
+    return a + b
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    a, b = sys.argv[1], sys.argv[2] # python execute param1
